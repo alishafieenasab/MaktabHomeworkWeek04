fe0a1a87334fc3813a4112893abb5b2e1456fd99
Ali <Ali@DESKTOP-91LMIMA>
2022-04-10 14:52:56 +0430

    Add project files.

---
+++ MaktabHomeworkWeek04/Person.cs
@@ -0,0 +1,35 @@
+﻿public class Person
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Id { get; set; }
+    public string? Height { get; set; }
+    public string Weight { get; set; }
+    public string? Mobile { get; set; }
+    public string? BirthDate { get; set; }
+    public string? FatherName { get; set; }
+    public string? Address { get; set; }
+
+    public Person(string firstName, string lastName, string id, string height, string weight, string mobile, string birthDate, string fatherName, string address)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Id = id;
+        Height = height;
+        Weight = weight;
+        Mobile = mobile;
+        BirthDate = birthDate;
+        FatherName = fatherName;
+        Address = address;
+        
+    }
+    public Person()
+    {
+
+    }
+    public override string ToString()
+    {
+        return $"{FirstName} - {LastName} - {Id} ";
+    }
+}
+
---
+++ MaktabHomeworkWeek04/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+Console.WriteLine(" Enter an Integer : ");
+int count = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter First Name : ");
+Console.WriteLine("Enter Last Name : ");
+Console.WriteLine("Enter Id : ");
+Console.WriteLine("Enter Height : ");
+Console.WriteLine("Enter Weight : ");
+Console.WriteLine("Enter Mobile : ");
+Console.WriteLine("Enter Birth Date : ");
+Console.WriteLine("Enter Father Name : ");
+Console.WriteLine("Enter Address : ");
+string? filePath = @"D:\tamrinFile\TamrinFile.txt";
+using (var stream = new FileStream(filePath, FileMode.Truncate))
+{
+    using (var writer = new StreamWriter(stream))
+    {
+        writer.Write("");
+    }
+}
+for (int i = 0; i < count; i++)
+{
+    string? aFirstName = Console.ReadLine();
+    string? aLastName = Console.ReadLine();
+    string? aId = Console.ReadLine();
+    string? aHeight = Console.ReadLine();
+    string? aWeight = Console.ReadLine();
+    string? aMobile = Console.ReadLine();
+    string? aBirthDate = Console.ReadLine();
+    string? aFatherName = Console.ReadLine();
+    string? aAddress = Console.ReadLine();
+    Person person = new Person(aFirstName, aLastName, aId, aHeight, aWeight, aMobile, aBirthDate, aFatherName, aAddress);
+    File.AppendAllText(filePath, person.FirstName + " - ");
+    File.AppendAllText(filePath, person.LastName + " - ");
+    File.AppendAllText(filePath, person.Id + " - ");
+    File.AppendAllText(filePath, person.Height + " - ");
+    File.AppendAllText(filePath, person.Weight + " - ");
+    File.AppendAllText(filePath, person.Mobile + " - ");
+    File.AppendAllText(filePath, person.BirthDate + " - ");
+    File.AppendAllText(filePath, person.FatherName + " - ");
+    File.AppendAllText(filePath, person.Address + "\n");
+ }
+Console.WriteLine("Do u want to see that info ? y/n");
+Person person1 = new Person();
+var check = Console.ReadLine();
+var lines = File.ReadAllLines(filePath);
+if (check == "y")
+{
+    var list = lines.Select(p => new Person()
+    {
+        FirstName = p.Split('-')[0].Trim(),
+        LastName = p.Split('-')[1].Trim(),
+        Id = p.Split('-')[2].Trim()
+    }).ToList();
+    foreach (var item in list)
+    {
+        Console.WriteLine(item);
+    }
+    using (var stream = new FileStream(filePath, FileMode.Truncate))
+    {
+        using (var writer = new StreamWriter(stream))
+        {
+            writer.Write("");
+        }
+    }
+}
+else if (check == "n")
+{
+    using (var stream = new FileStream(filePath, FileMode.Truncate))
+    {
+        using (var writer = new StreamWriter(stream))
+        {
+            writer.Write("");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Invalid op");
+    using (var stream = new FileStream(filePath, FileMode.Truncate))
+    {
+        using (var writer = new StreamWriter(stream))
+        {
+            writer.Write("");
+        }
+    }
+}
